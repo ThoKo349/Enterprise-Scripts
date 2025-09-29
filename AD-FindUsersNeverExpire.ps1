21cbc4082703420b65fc4440d1d0349b39bb658d
ThoKo349 <mail@tkoller.de>
2025-09-29 11:54:50 +0200

    Delete AD-FindUsersNeverExpire.ps1

--- AD-FindUsersNeverExpire.ps1
+++
@@ -1,9 +0,0 @@
-<#
-.SYNOPSIS
-  Listet Benutzer, deren Passwort nie abläuft.
-#>
-Import-Module ActiveDirectory
-
-Get-ADUser -Filter * -Properties PasswordNeverExpires |
-  Where-Object { $_.PasswordNeverExpires -eq $true } |
-  Select-Object SamAccountName, DisplayName, Enabled
